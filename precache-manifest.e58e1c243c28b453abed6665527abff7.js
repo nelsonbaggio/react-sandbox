2d40c940f757f9592468f64ba9386b9dded627b2
Nelson Baggio <nelson.baggio@gmail.com>
2020-07-26 01:42:35 -0300

    Updates

---
+++ precache-manifest.e58e1c243c28b453abed6665527abff7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6979053140ae3b8def0943d58cb85a",
+    "url": "/react-sandbox/index.html"
+  },
+  {
+    "revision": "d0c79e89fc8fe1ba64a5",
+    "url": "/react-sandbox/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fdcab2610e626414cde3",
+    "url": "/react-sandbox/static/js/2.067f0017.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sandbox/static/js/2.067f0017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c79e89fc8fe1ba64a5",
+    "url": "/react-sandbox/static/js/main.b14e6c9c.chunk.js"
+  },
+  {
+    "revision": "31d9f364dfb7b4daa505",
+    "url": "/react-sandbox/static/js/runtime-main.16b66f98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-sandbox/static/media/logo.5d5d9eef.svg"
+  }
+]);
